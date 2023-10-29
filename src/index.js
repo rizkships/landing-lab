742aa6cdbae450fdbd199a190c58af6194a6c343
rizk <rizkdotai@gmail.com>
2023-10-29 18:18:06 +0200

    setup basic styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-console.log('omar')
+import "./styles.css"
+
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,20 @@ module.exports = {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
     },
-};
+    module: {
+        rules: [
+        {
+        test: /\.css$/, 
+        use:[
+            'style-loader', 
+            {loader: 'css-loader', 
+            options: {importLoaders: 1}},
+             'postcss-loader']
+        }
+        ]
+        },
+        devServer: { watchContentBase: true, contentBase:path.resolve(__dirname, 'dist'), 
+        open: true
+        }
+        };
+        
